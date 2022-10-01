90d5dc26b2c35faf8cb5333d84dbbd31fd1c659e
As9124k82134 <akbaseren956@gmail.com>
2022-10-01 18:25:53 +0300

    apps_clean: Update debloat

--- apps_clean/oppo.sh
+++ apps_clean/oppo.sh
@@ -12,12 +12,8 @@ rm -rf $1/../my_heytap/priv-app/*Auto*
 rm -rf $1/../my_preload/app/*
 rm -rf $1/../my_preload/del-app/*
 rm -rf $1/../my_region/*app/*
-rm -rf $1/../my_stock/app/*Game*
 rm -rf $1/../my_stock/app/*KeKe*
 rm -rf $1/../my_stock/app/*Karaoke*
-rm -rf $1/../my_stock/app/*Recorder*
-rm -rf $1/../my_stock/app/*Music*
-rm -rf $1/../my_stock/app/*Compass*
 rm -rf $1/../my_stock/app/*Pay*
 rm -rf $1/../my_stock/app/*Health*
 rm -rf $1/../my_stock/app/*realmeMovie*
@@ -30,7 +26,6 @@ rm -rf $1/fonts/NotoSans*
 rm -rf $1/fonts/NotoSerif*
 rm -rf $1/*/*app/*Health*
 rm -rf $1/*/*app/*OTA*
-rm -rf $1/system_ext/media/notifications/*
 
 rm -rf $1/preload/*
 rm -rf $1/app/BackupRestore*
@@ -39,8 +34,6 @@ rm -rf $1/app/Calculator2
 rm -rf $1/app/ChildrenSpace
 rm -rf $1/app/EmailPartnerProvider
 rm -rf $1/app/FindMyPhoneClient
-rm -rf $1/*app/*Game*
-rm -rf $1/app/Ocrscanner
 rm -rf $1/app/Oshare
 rm -rf $1/app/OV*
 rm -rf $1/app/com.*
@@ -50,13 +43,9 @@ rm -rf $1/app/ModemTestMode
 rm -rf $1/app/talkback
 rm -rf $1/*app/*FM*
 rm -rf $1/*app/*Recorder*
-rm -rf $1/app/OppoCamera
-rm -rf $1/app/OppoCompass2
 rm -rf $1/app/OppoDCS
 rm -rf $1/app/Youtube
 rm -rf $1/app/SecurePay
-rm -rf $1/app/SecureElement
-rm -rf $1/app/SafeCenter
 rm -rf $1/priv-app/BlackList
 rm -rf $1/priv-app/BlackListApp
 rm -rf $1/priv-app/*Browser*
@@ -69,12 +58,8 @@ rm -rf $1/priv-app/deezer*
 rm -rf $1/priv-app/TestApp5G
 rm -rf $1/priv-app/com.facebook.system
 rm -rf $1/priv-app/SmartDrive
-rm -rf $1/priv-app/OppoMusic
 rm -rf $1/priv-app/OppoBootReg
-rm -rf $1/priv-app/OppoGallery2
-rm -rf $1/priv-app/VideoGallery
 rm -rf $1/priv-app/Velvet
-rm -rf $1/priv-app/VideoGallery
 rm -rf $1/reserve
 rm -rf $1/app/com.amazon.appmanager
 rm -rf $1/*app/*facebook*
@@ -85,10 +70,6 @@ rm -rf $1/priv-app/KeKeMarket
 rm -rf $1/priv-app/KeKePay
 rm -rf $1/priv-app/KeKeUserCenter
 rm -rf $1/app/NXPNfcNci
-rm -rf $1/app/OcrScanner
 rm -rf $1/app/OcrService
-rm -rf $1/media/audio/ringtones/ringtone_01*
-rm -rf $1/media/audio/ringtones/ringtone_02*
-rm -rf $1/media/audio/notifications/notification_01*
 rm -rf $1/fonts/D*
 rm -rf $1/fonts/Source*
